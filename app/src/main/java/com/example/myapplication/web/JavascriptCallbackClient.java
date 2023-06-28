2285afd479979ec29181c631983755685846059b
김영선 <withbbang@gmail.com>
2023-06-28 17:47:44 +0900

    first commit

---
+++ app/src/main/java/com/example/myapplication/web/JavascriptCallbackClient.java
@@ -0,0 +1,46 @@
+package com.example.myapplication.web;
+
+import android.app.Activity;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebView;
+import android.widget.Toast;
+
+public class JavascriptCallbackClient {
+
+    private Activity mContext;
+    private WebView webView;
+
+    public JavascriptCallbackClient(Activity activity, WebView webView) {
+        this.mContext = activity;
+        this.webView = webView;
+    }
+
+    private String publishEvent(String functionName, String data) {
+        StringBuffer buffer = new StringBuffer()
+                .append("window.dispatchEvent(\n")
+                .append("   new CustomEvent(\"").append(functionName).append("\", {\n")
+                .append("           detail: {\n")
+                .append("               data: ").append(data).append("\n")
+                .append("           }\n")
+                .append("       }\n")
+                .append("   )\n")
+                .append(");");
+        return buffer.toString();
+    }
+
+    @JavascriptInterface
+    public void showToastMessage(final String message) {
+        Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show();
+    }
+
+    @JavascriptInterface
+    public void showAlertMessage() {
+        webView.postDelayed(() -> {
+            webView.evaluateJavascript(publishEvent("showAlertMessage", "\"Hello, I'm message from Android\""),
+                    (result) -> {
+                        Toast.makeText(mContext, result, Toast.LENGTH_SHORT).show();
+                    }
+            );
+        }, 5000);
+    }
+}
